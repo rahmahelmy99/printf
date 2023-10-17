05b8390bd0167d5ac8eddaec9fb65bde5e773e71
rahma <helmyrahma4@gmail.com>
2023-10-17 13:23:03 -0700

    updated

--- _printf.c
+++ _printf.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
-void select_sp(const char *format, va_list args, int count);
 /**
  * _printf - prototype function that produces output according
  * to a format.
@@ -21,7 +20,29 @@ int _printf(const char *format, ...)
 		if (*format == '%')
 		{
 			format++;
-			select_sp(format, args, count);
+			switch (*format)
+			{
+				case 'c':
+					is_char(va_arg(args, int), &count);
+					break;
+				case 's':
+					is_string(va_arg(args, char *), &count);
+					break;
+				case '%':
+					write(1, "%", 1);
+					count++;
+					break;
+				case 'd':
+					is_integer(va_arg(args, int), &count);
+					break;
+				case 'i':
+					is_integer(va_arg(args, int), &count);
+					break;
+				default:
+					write(1, format - 1, 2);
+					count += 2;
+					break;
+			}
 		}
 		else
 		{
@@ -32,37 +53,4 @@ int _printf(const char *format, ...)
 	}
 	va_end(args);
 	return (count);
-}
-/**
- * select_sp - select speciefier of the format.
- * @format: format.
- * @args:array of args.
- * @count: pointer to counter
- * Return: nothing.
-*/
-void select_sp(const char *format, va_list args, int count)
-{
-	switch (*format)
-	{
-		case 'c':
-			is_char(va_arg(args, int), &count);
-			break;
-		case 's':
-			is_string(va_arg(args, char *), &count);
-			break;
-		case '%':
-			write(1, "%", 1);
-			count++;
-			break;
-		case 'd':
-			is_integer(va_arg(args, int), &count);
-			break;
-		case 'i':
-			is_integer(va_arg(args, int), &count);
-			break;
-		default:
-			write(1, format - 1, 2);
-			count += 2;
-			break;
-	}
 }
