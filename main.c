58671a310991aa004bf0d4b16d617fe2e26326b5
rahma <helmyrahma4@gmail.com>
2023-10-17 08:35:59 -0700

    added

--- main.c
+++ main.c
@@ -20,12 +20,11 @@ int main(void)
 	printf("String:[%s]\n", "I am a string !");
 	_printf("Percent:[%%]\n");
 	printf("Percent:[%%]\n");
-	/*
-	 * _printf("Negative:[%d]\n", -762534);
-	 * printf("Negative:[%d]\n", -762534);
-	 * _printf("Length:[%d, %i]\n", len, len);
-	 * printf("Length:[%d, %i]\n", len2, len2);
-	*/
+	_printf("Negative:[%d]\n", -762534);
+	printf("Negative:[%d]\n", -762534);
+	_printf("Length:[%d, %i]\n", len, len);
+	printf("Length:[%d, %i]\n", len2, len2);
+
 	len = _printf("Percent:[%%]\n");
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
